b83d47a597a4fb551152f2d4c21cc77332934374
Leandro W. Konopka <gamesle@hotmail.com>
2021-01-20 10:47:24 +0000

    Delete IdentityUser Options

--- BulkyBook/Startup.cs
+++ BulkyBook/Startup.cs
@@ -31,7 +31,7 @@ namespace BulkyBook
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
         }
